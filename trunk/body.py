19cdae22b078b4bad0969b330535b7f6cfb42848
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2007-02-25 17:18:47 +0000

    visually recursive now

--- trunk/body.py
+++ trunk/body.py
@@ -1,17 +1,33 @@
 from UserList import UserList
 from OpenGL.GL import *
 class Body:
-	def __init__(self):
+	def __init__(self, scale, orig, dest):
+		# orig and dest are just y,z points - we're not altering the x positions
+		self.set_vars(scale, orig, dest)
 		self.body_pts = []
 		self.read_body()
+
+	def set_vars(self, s, orig, dest, pos=None):
+		self.scale = s
+		self.init = orig
+		self.dest = dest
+		self.ctl = (dest[0], orig[1]) # control point for trajectory curve
+		self.pos = pos
+		if not pos:	
+			self.pos = orig
 	
 	def draw(self):
 		glColor3f(1.0, 1.0, 1.0)
+		glPushMatrix()
+		glRotatef(270.0,1.0,0.0,0.0)
+		glTranslatef(0.0, self.pos[0], self.pos[1]*self.scale)
+		glScalef(self.scale, self.scale, self.scale)
 		glBegin(GL_POINTS)
 		for pt in self.body_pts:
 			pt.draw()
 
 		glEnd();				
+		glPopMatrix()
 
 	def read_body(self):
 		pf = open("pts")
@@ -22,9 +38,20 @@ class Body:
 		# zsort so that we go from the head down
 		self.body_pts.sort(cmp=lambda x,y: int(y[2]*1000)-int(x[2]*1000))
 		
-
 class BodyPoint(UserList):
 	def __init__(self, xyz):
 		self.data = xyz
 	def draw(self):
 		glVertex3f(*self.data)
+
+
+# def load_textures():
+	# texturefile = os.path.join('data','nehe.bmp')
+	# textureSurface = pygame.image.load(texturefile)
+	# textureData = pygame.image.tostring(textureSurface, "RGBX", 1)
+	# 
+	# glBindTexture(GL_TEXTURE_2D, textures[0])
+	# glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, textureSurface.get_width(), textureSurface.get_height(), 0,
+		# GL_RGBA, GL_UNSIGNED_BYTE, textureData );
+	# glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+	# glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
