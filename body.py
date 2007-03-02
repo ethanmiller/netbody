6dc7d6c8cc0a7e5c23660ee9c77024e49d15ab1c
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2007-03-02 06:38:21 +0000

    messier code, but cleaner viz + new body

--- body.py
+++ body.py
@@ -30,8 +30,8 @@ class Body:
 
 	def read_body(self):
 		pf = open("pts")
-		crownpts = 81 # how many of thes points constitute the top  of the head
-		platformpts = 145 # how many points make up the "landscape"
+		crownpts = 275 # how many of thes points constitute the top  of the head
+		platformpts = 2779 # how many points make up the "landscape"
 		# realines() on this file produces a list of strings in the format: '1.000000 1.0000000 2.000000\n'
 		body_pts_tmp = [x[:-1].split(' ') for x in pf.readlines()] 
 		body_pts_tmp = [[float(y) for y in x] for x in body_pts_tmp]
@@ -51,10 +51,10 @@ class BodyPoint(UserList):
 		self.data = xyz
 		# pt_type : 0 = regular body point, 1 = "landscape" point, 2 = crown point
 		self.pt_type = type
-		if type == 1:
-			self.images = [ImagePlane(self.data, type==2 and landscape_mode, type==1) for x in range(1)]
-		else:
-			self.images = [ImagePlane(self.data, type==2 and landscape_mode, type==1) for x in range(5)]
+		#if type == 1:
+		self.images = [ImagePlane(self.data, type==2 and landscape_mode, type==1) for x in range(1)]
+		#else:
+		#	self.images = [ImagePlane(self.data, type==2 and landscape_mode, type==1) for x in range(5)]
 	
 	def draw(self):
 		for i in self.images:
@@ -78,7 +78,7 @@ class ImagePlane:
 			if landscape:
 				# scale so that we're smaller closer to center
 				maxdist = 1.1 # based on the current body model
-				maxscale = 0.5
+				maxscale = 0.1
 				dist = math.sqrt(dest[0]*dest[0] + dest[1]*dest[1])			
 				self.color = (1.0, 0.0, 0.0, 0.5)
 				# two points (maxdist, maxscale), (0, .002)
@@ -89,6 +89,7 @@ class ImagePlane:
 				b = maxscale - slope*maxdist
 				# for a given x
 				self.scale = slope*dist + b
+				self.scale = 0.1
 			else:
 				self.scale = 1.0
 
@@ -99,13 +100,14 @@ class ImagePlane:
 		glPushMatrix()
 		glTranslatef(*self.pos)
 		glScalef(self.scale, self.scale, self.scale)
-		glRotatef(self.angle, *self.rotvec)
+		glRotatef(90, *self.rotvec)
+		#glRotatef(self.angle, *self.rotvec)
 		glBegin(GL_POLYGON)
 		# doing a 3x4 rec for now
 		glVertex3f(0.0, 0.0, 0.0)
-		glVertex3f(0.3, 0.0, 0.0)
-		glVertex3f(0.3, 0.4, 0.0)
-		glVertex3f(0.0, 0.4, 0.0)
+		glVertex3f(1.5, 0.0, 0.0)
+		glVertex3f(1.5, 2.0, 0.0)
+		glVertex3f(0.0, 2.0, 0.0)
 		glVertex3f(0.0, 0.0, 0.0)
 		glEnd()
 		glPopMatrix()
