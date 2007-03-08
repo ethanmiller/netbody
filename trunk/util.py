8a1f6f2983638c88e11d7f51c72c2f8edd4b7258
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2007-03-08 06:20:39 +0000

    got images applied - but depends on prepopulating image directory..

--- trunk/util.py
+++ trunk/util.py
@@ -1,6 +1,7 @@
 import math
 
 class CONST:
+	SAVE_FRAMES = True
 	# general visualization constants
 	VIZSIZE = (800, 450)	
 	BGCOLOR = (0.9, 0.9, 0.85, 0.0)
@@ -15,16 +16,29 @@ class CONST:
 	TITLE_NEWS_TOP = 'news_top'
 	TITLE_NEWS_PHOTOS = 'news_photos'
 	# image/text object constants
-	BIT_DRIFT_LIMIT = 10
-	BIT_TRAJ_VEC_RANGE = (-0.05, 0.05)
+	#BIT_DRIFT_LIMIT = 10 # [[[testing value]]]
+	BIT_DRIFT_LIMIT = 180 
+	#BIT_TRAJ_VEC_RANGE = (-0.05, 0.05) # [[[testing value]]]
+	BIT_TRAJ_VEC_RANGE = (-0.005, 0.005)
 	BIT_STUCK_LIMIT = 50000
 	BIT_UNSTUCK_ALPHA = 0.2
 	BIT_ROT_INC_RANGE = (1.0, 2.0)
 	# "attention" block constants
 	ATT_ANI_STEPS = 16.0
-	ATT_POS_INDX_START = 4800
+	ATT_POS_INDX_START = 5000
 	ATTCOLOR = (1.0, 0.0, 0.0)
 
 def distance(pta, ptb):
 	diff = [x[1] - x[0] for x in zip(pta, ptb)]
 	return math.sqrt(reduce(lambda x, y: x+y, [z*z for z in diff]))
+
+def pow2(n):
+	n = n -1
+	n = n | (n >> 1)
+	n = n | (n >> 2)
+	n = n | (n >> 4)
+	n = n | (n >> 8)
+	n = n | (n >> 16)
+	n = n | (n >> 32)
+	n = n + 1
+	return n
