ac529b62e3503bcf046687054cb5d5341c97eebb
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2007-03-09 06:36:09 +0000

    gave manager more responsibilities

--- trunk/collector.py
+++ trunk/collector.py
@@ -8,7 +8,9 @@ class Collector:
 	def data(self):
 		images = filter(lambda x: not x.startswith('.'), os.listdir('resources/images'))
 		# TODO do this intelligently
-		return ['resources/images/%s' % random.choice(images) for x in range(random.randint(0, 1))]
+		# data structure for an image + associated words.. possibly more than one...
+		# [{'img' : '/path/to/image', 'words' : ['a', 'b', 'c']}, {..}, ...]
+		return [{'img' : 'resources/images/%s' % random.choice(images), 'words' : ['aa', 'bb']} for x in range(random.randint(0, 1))]
 
 	def proc_json(self):
 		feed = urllib.urlopen(util.CONST.FEED_URL)
--- trunk/panel.py
+++ trunk/panel.py
@@ -7,7 +7,7 @@ class Panel:
 		self.time_tex = 0
 
 	def draw(self, dat, atat):
-		#at = self.screen_att(atat)
+
 		glColor4f(0.6, 0.6, 0.55, 1.0)
 		glBegin(GL_QUADS)
 		glVertex2f(util.CONST.PANEL_PADDING, util.CONST.PANEL_PADDING)
@@ -24,19 +24,19 @@ class Panel:
 		glVertex3f(*atat)
 		glEnd()
 
-		stime = time.strftime('%a, %b %d %H:%M:%S')
-		im = Image.new('RGB', (128, 32), (255, 255, 255))
-		drawr = ImageDraw.Draw(im)
-		drawr.text((5, 15), stime, font=self.font, fill=0)
+		#stime = time.strftime('%a, %b %d %H:%M:%S')
+		#im = Image.new('RGB', (128, 32), (255, 255, 255))
+		#drawr = ImageDraw.Draw(im)
+		#drawr.text((5, 15), stime, font=self.font, fill=0)
 
-		glBindTexture(GL_TEXTURE_2D, self.time_tex)
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, im.size[0], im.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, im.tostring())
-		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-		glBegin(GL_QUADS)
-		glTexCoord2f(0.0, 0.0); glVertex3f(5.0, 408.0, 0.0)
-		glTexCoord2f(1.0, 0.0); glVertex3f(133.0, 408.0, 0.0)
-		glTexCoord2f(1.0, 1.0); glVertex3f(133.0, 440.0, 0.0)
-		glTexCoord2f(0.0, 1.0); glVertex3f(5, 440.0, 0.0)
-		glEnd()
+		#glBindTexture(GL_TEXTURE_2D, self.time_tex)
+		#glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, im.size[0], im.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, im.tostring())
+		#glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+		#glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+		#glBegin(GL_QUADS)
+		#glTexCoord2f(0.0, 0.0); glVertex3f(5.0, 408.0, 0.0)
+		#glTexCoord2f(1.0, 0.0); glVertex3f(133.0, 408.0, 0.0)
+		#glTexCoord2f(1.0, 1.0); glVertex3f(133.0, 440.0, 0.0)
+		#glTexCoord2f(0.0, 1.0); glVertex3f(5, 440.0, 0.0)
+		#glEnd()
 
