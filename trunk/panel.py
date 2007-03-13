e71212de68d4f67a74a11fe0897836c9c25ab56a
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2007-03-13 03:06:59 +0000

    all parts except the log, still have that image sidebar glitch

--- trunk/panel.py
+++ trunk/panel.py
@@ -3,15 +3,19 @@ import util, time, Image, ImageDraw, ImageFont, math
 
 class Panel:
 	def __init__(self):
-		self.font = ImageFont.truetype('resources/NewMedia.ttf', 12)
+		self.font = ImageFont.truetype('resources/NewMedia.ttf', 16)
 		self.imgtex = glGenTextures(1)
 		self.imgids = []
-		self.imgwidth = util.pow2(util.CONST.PANEL_WIDTH)
-		self.imgheight = 0
-		self.imgpanel = Image.new('RGB', (self.imgwidth, util.pow2(util.CONST.VIZSIZE[1])), util.CONST.PANEL_BG)
+		self.imgrealheight = 0
+		self.imgpanelsize = [util.pow2(util.CONST.IPANEL_WIDTH), util.pow2(util.CONST.VIZSIZE[1])]
+		self.imgpanel = Image.new('RGB', (self.imgpanelsize[0], self.imgpanelsize[1]), util.CONST.PANEL_BG)
 		self.imgoffset = 0
-		self.imgretex = True
-		self.wordpanel = None
+		self.retexture = True
+		self.wordrealsize = 0
+		self.wordpanelsize = [util.pow2(util.CONST.VIZSIZE[0]), util.pow2(util.CONST.WPANEL_HEIGHT)]
+		self.wordtex = glGenTextures(1)
+		self.wordpanel = Image.new('RGB', (self.wordpanelsize[0], self.wordpanelsize[1]), util.CONST.PANEL_BG)
+		self.wordoffset = 0
 
 	def draw(self, dat, atat):
 		for d in dat:
@@ -23,73 +27,112 @@ class Panel:
 
 	def draw_images(self):
 		glBindTexture(GL_TEXTURE_2D, self.imgtex)
-		if self.imgretex:
+		if self.retexture:
 			strrep = self.imgpanel.tostring()
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, self.imgpanel.size[0], self.imgpanel.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, strrep)
 			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
 			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-			self.imgretex = False
-		panelheight = util.CONST.VIZSIZE[1] - util.CONST.PANEL_PADDING * 2
+		panelheight = util.CONST.VIZSIZE[1] - util.CONST.IPANEL_PADDING * 2
 		propytop = 0.0
 		if self.imgoffset > 0.0:
 			propytop = (self.imgoffset*1.0)/self.imgpanel.size[1]
 		glColor4f(1.0, 1.0, 1.0, 1.0)
 		glBegin(GL_QUADS)
 		glTexCoord2f(0.0, propytop)
-		glVertex2f(util.CONST.PANEL_PADDING, util.CONST.PANEL_PADDING)
-		glTexCoord2f(util.CONST.PANEL_WIDTH/(self.imgpanel.size[0]*1.0), propytop)
-		glVertex2f(util.CONST.PANEL_WIDTH + util.CONST.PANEL_PADDING, util.CONST.PANEL_PADDING)
-		glTexCoord2f(util.CONST.PANEL_WIDTH/(self.imgpanel.size[0]*1.0), (panelheight+self.imgoffset)/self.imgpanel.size[1])
-		glVertex2f(util.CONST.PANEL_WIDTH + util.CONST.PANEL_PADDING, panelheight)
-		glTexCoord2f(0.0, (panelheight+self.imgoffset)/self.imgpanel.size[1])
-		glVertex2f(util.CONST.PANEL_PADDING,  panelheight)
+		glVertex2f(util.CONST.IPANEL_PADDING, util.CONST.IPANEL_PADDING)
+		glTexCoord2f(util.CONST.IPANEL_WIDTH/(self.imgpanel.size[0]*1.0), propytop)
+		glVertex2f(util.CONST.IPANEL_WIDTH + util.CONST.IPANEL_PADDING, util.CONST.IPANEL_PADDING)
+		glTexCoord2f(util.CONST.IPANEL_WIDTH/(self.imgpanel.size[0]*1.0), (panelheight+self.imgoffset*1.0)/self.imgpanel.size[1])
+		glVertex2f(util.CONST.IPANEL_WIDTH + util.CONST.IPANEL_PADDING, panelheight)
+		glTexCoord2f(0.0, (panelheight+self.imgoffset*1.0)/self.imgpanel.size[1])
+		glVertex2f(util.CONST.IPANEL_PADDING,  panelheight)
 		glEnd()
 		if self.imgoffset > 0:
 			if self.imgoffset < 0.2:
 				self.imgoffset = 0.0
 				# chop off the image if it has gotten long
-				self.imgsize = util.CONST.VIZSIZE[1]
+				self.imgpanelsize[1] = util.CONST.VIZSIZE[1]
+				self.imgrealheight = self.imgpanelsize[1]
 			else:
 				self.imgoffset -= self.imgoffset/6.0
 
 	def draw_words(self):
-		pass
+		glBindTexture(GL_TEXTURE_2D, self.wordtex)
+		if self.retexture:
+			strrep = self.wordpanel.tostring()
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, self.wordpanel.size[0], self.wordpanel.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, strrep)
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+			self.retexture = False
+		panelwidth = util.CONST.VIZSIZE[0] - util.CONST.WPANEL_PADDING * 2
+		propx = 0.0
+		if self.wordoffset > 0.0:
+			propx = (self.wordoffset*1.0)/self.wordpanel.size[0]
+		glColor4f(1.0, 1.0, 1.0, 1.0)
+		glBegin(GL_QUADS)
+		glTexCoord2f(propx, 0.0)
+		glVertex2f(util.CONST.WPANEL_PADDING, util.CONST.VIZSIZE[1]-util.CONST.WPANEL_PADDING-util.CONST.WPANEL_HEIGHT)
+		glTexCoord2f((panelwidth+self.wordoffset)/self.wordpanel.size[0], 0.0)
+		glVertex2f(panelwidth, util.CONST.VIZSIZE[1]-util.CONST.WPANEL_PADDING-util.CONST.WPANEL_HEIGHT)
+		glTexCoord2f((panelwidth+self.wordoffset)/self.wordpanel.size[0], util.CONST.WPANEL_HEIGHT/(self.wordpanel.size[1]*1.0))
+		glVertex2f(panelwidth, util.CONST.VIZSIZE[1]-util.CONST.WPANEL_PADDING)
+		glTexCoord2f(propx, util.CONST.WPANEL_HEIGHT/(self.wordpanel.size[1]*1.0))
+		glVertex2f(util.CONST.WPANEL_PADDING, util.CONST.VIZSIZE[1]-util.CONST.WPANEL_PADDING)
+		glEnd()
+		if self.wordoffset > 0:
+			if self.wordoffset < 0.2:
+				self.wordoffset = 0.0
+				# chop
+				self.wordpanelsize[0] = util.CONST.VIZSIZE[0]
+				self.wordrealsize = self.wordpanelsize[0]
+			else:
+				self.wordoffset -= self.wordoffset/6.0
 
 	def addimagewords(self, d):
-		self.imgheight += d['img'].size[1]
-		minh = int(math.ceil(self.imgheight))
-		if (minh - self.imgoffset) < util.CONST.VIZSIZE[1]:
-			minh = int(math.ceil(util.CONST.VIZSIZE[1] + self.imgoffset + d['img'].size[1]))
+		# --------- image ----------------
+		self.imgrealheight += d['img'].size[1]
+		self.imgpanelsize[1] = util.pow2(self.imgrealheight)
+		while self.imgpanelsize[1] < (util.CONST.VIZSIZE[1] + self.imgoffset):
+			self.imgpanelsize[1] = util.pow2(self.imgpanelsize[1] + 1) # round up to the next power of 2
+		#*** bites it any larger than 2048
+		if self.imgpanelsize[1] > 2048:
+			self.imgpanelsize[1] = 2048
 		#this is a blank panel with the new image height added
-		tmppanel = Image.new('RGB', (self.imgwidth, util.pow2(minh)), util.CONST.PANEL_BG)
+		tmppanel = Image.new('RGB', (self.imgpanelsize[0], self.imgpanelsize[1]), util.CONST.PANEL_BG)
 		#paste the new image first, and the old set below that.
 		tmppanel.paste(d['img'], (0,0))
 		tmppanel.paste(self.imgpanel, (0, d['img'].size[1]))
 		self.imgpanel = tmppanel
 		self.imgoffset += d['img'].size[1]
-		self.imgretex = True
-
-		#at_xdiff = atat[0] - util.CONST.PANEL_WIDTH
-		#glBegin(GL_LINE_STRIP)
-		#glVertex3f(util.CONST.PANEL_WIDTH, 430.0, 0.0)
-		#glVertex3f(at_xdiff/2.0 + util.CONST.PANEL_WIDTH, 430.0, 0.0)
-		#glVertex3f(at_xdiff/2.0 + util.CONST.PANEL_WIDTH, atat[1], 0.0)
-		#glVertex3f(*atat)
-		#glEnd()
-
-		#stime = time.strftime('%a, %b %d %H:%M:%S')
-		#im = Image.new('RGB', (128, 32), (255, 255, 255))
-		#drawr = ImageDraw.Draw(im)
-		#drawr.text((5, 15), stime, font=self.font, fill=0)
-
-		#glBindTexture(GL_TEXTURE_2D, self.time_tex)
-		#glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, im.size[0], im.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, im.tostring())
-		#glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-		#glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-		#glBegin(GL_QUADS)
-		#glTexCoord2f(0.0, 0.0); glVertex3f(5.0, 408.0, 0.0)
-		#glTexCoord2f(1.0, 0.0); glVertex3f(133.0, 408.0, 0.0)
-		#glTexCoord2f(1.0, 1.0); glVertex3f(133.0, 440.0, 0.0)
-		#glTexCoord2f(0.0, 1.0); glVertex3f(5, 440.0, 0.0)
-		#glEnd()
-
+		# ----------- words -------------
+		wordimgs = []
+		tmppanel = Image.new('RGB', (8, 8), (0.0, 0.0, 0.0)) # using this just to get textsize
+		drawr = ImageDraw.Draw(tmppanel)
+		newwordwidth = 0
+		for w in d['words']:	
+			wit = drawr.textsize(w, font=self.font)
+			newwordwidth = newwordwidth + wit[0] + util.CONST.WORD_PADDING
+			# now that I know the size, I can build it
+			wim = Image.new('RGB', (wit[0]+2, wit[1]+2), util.CONST.PANEL_BG)
+			dr = ImageDraw.Draw(wim)
+			dr.text((1, 1), w, font=self.font, fill=0)
+			wordimgs.append(wim)
+		# ok - now the tmppanel
+		self.wordrealsize += newwordwidth
+		self.wordpanelsize[0] = util.pow2(int(self.wordrealsize))
+		while self.wordpanelsize[0] < (util.CONST.VIZSIZE[0] + self.wordoffset):
+			self.wordpanelsize[0] = util.pow2(self.wordpanelsize[0] + 1)
+		#*** bites it any larger than 2048
+		if self.wordpanelsize[0] > 2048:
+			self.wordpanelsize[0] = 2048
+		tmppanel = Image.new('RGB', (self.wordpanelsize[0], self.wordpanelsize[1]), util.CONST.PANEL_BG)
+		wordx = 0.0
+		for wi in wordimgs:
+			tmppanel.paste(wi, (int(wordx), 5))
+			wordx = wordx + wi.size[0] + util.CONST.WORD_PADDING
+		# paste the old image
+		tmppanel.paste(self.wordpanel, (int(wordx), 0))
+		self.wordpanel = tmppanel
+		self.wordoffset += newwordwidth
+		# ---- retexture ----	
+		self.retexture = True
