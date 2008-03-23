f1ca13fb79e18965f86a34461d7e3020bf59a6a9
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2008-03-23 02:03:59 +0000

    got some timing, and more spidering - working through each entity spidering...

--- trunk/apis/delic.py
+++ trunk/apis/delic.py
@@ -1,6 +1,39 @@
 import feedparser
 
+mark = 0
+
 def by_tag(t):
+	pause()
 	fobj = feedparser.parse("http://del.icio.us/rss/popular/%s" % t)
 	# keys in entries: ['updated', 'updated_parsed', 'links', 'author', 'title', 'title_detail', 'link', 'id', 'tags']
 	return fobj['entries']
+
+def url_data(url):
+	pause()
+	fobj = feedparser.parse(url_feed(url))
+	# username is entries['author'], for tags entries['tags'] =  [{'term': 'tag othertag', 'scheme': None, 'label': None}]
+	return fobj['entries']
+
+def url_feed(url):
+	import hashlib
+	m = hashlib.md5()
+	m.update(url)
+	return "http://feeds.delicious.com/rss/url/%s" % m.hexdigest()
+
+def pause():
+	global mark
+	import time, util
+	elapsed = time.clock() - mark
+	if elapsed < util.CONST.SERVICE_HIT_PAUSE:
+		sleeptime = util.CONST.SERVICE_HIT_PAUSE - elapsed
+		print ".... pausing %s seconds for del.icio.us ...."
+		time.sleep(sleeptime)
+	mark = time.clock()
+
+#>>> import hashlib
+#>>> m = hashlib.md5()
+#>>> m.update("http://www.donotreply.com/")
+#>>> m.digest()
+#'gi\x10\xd4\x1a\x8e\xe5\xe0\x1b\x02\xe9E\xe02\xe9^'
+#>>> m.hexdigest()
+#'676910d41a8ee5e01b02e945e032e95e'
--- trunk/apis/flickr.py
+++ trunk/apis/flickr.py
@@ -4,9 +4,22 @@ import urllib, simplejson
 #api_key = '4b86073fd285073df5233297bd5f596b'
 #flickr = flickrapi.FlickrAPI(api_key)
 
+mark = 0
+
 def by_tag(t):
+	pause()
 	# public feed
 	feed = urllib.urlopen("http://api.flickr.com/services/feeds/photos_public.gne?tags=%s&format=json&nojsoncallback=1" % t)
 	jobj = simplejson.load(feed)
 	# keys in items: ['description', 'date_taken', 'title', 'media', 'author', 'link', 'published', 'author_id', 'tags']
 	return jobj['items']
+
+def pause():
+	global mark
+	import time, util
+	elapsed = time.clock() - mark
+	if elapsed < util.CONST.SERVICE_HIT_PAUSE:
+		sleeptime = util.CONST.SERVICE_HIT_PAUSE - elapsed
+		print ".... pausing %s seconds for flickr ...." % sleeptime
+		time.sleep(sleeptime)
+	mark = time.clock()
