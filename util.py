389acfc7d66830d642c1be84d0cdbd86e3f0f6eb
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2008-03-30 04:07:21 +0000

    some refactoring, better identity handling, but username only passively checks multi-identity

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import math, Image
 
 class CONST:
 	SAVE_FRAMES = False
-	SERVICE_HIT_PAUSE = 5 # how many seconds to pause between hitting apis/feeds
+	SERVICE_HIT_PAUSE = 10 # how many seconds to pause between hitting apis/feeds
 	WIN_WIDTH = 20
 	WIN_HEIGHT = 20
 	NOISE_WORDS = "about,after,all,also,an,and,another,any,are,as,at,be,because,been,before,being,between,both,but,by,came,can,come,could,did,do,each,for,from,get,got,has,had,he,have,her,here,him,himself,his,how,if,in,into,is,it,like,make,many,me,might,more,most,much,must,my,never,now,of,on,only,or,other,our,out,over,said,same,see,should,since,some,still,such,take,than,that,the,their,them,then,there,these,they,this,those,through,to,too,under,up,very,was,way,we,well,were,what,where,which,while,who,with,would,you,your,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,$,1,2,3,4,5,6,7,8,9,0,_".split(",")
@@ -32,12 +32,14 @@ def log(msg):
 
 def pause(for_whom, mark):
 	import time
-	elapsed = time.clock() - mark
+	elapsed = time.time() - mark
 	if elapsed < CONST.SERVICE_HIT_PAUSE:
 		sleeptime = CONST.SERVICE_HIT_PAUSE - elapsed
-		print ".... pausing %s seconds for %s ...." % (sleeptime, for_whom)
+		print ".... pausing %0.2f seconds for %s ...." % (sleeptime, for_whom)
 		time.sleep(sleeptime)
-	return time.clock()
+	else:
+		print ".... no pause for %s ...." % for_whom
+	return time.time()
 
 def unicode_enforce(obj, encoding='utf-8'):
 	if isinstance(obj, basestring):
