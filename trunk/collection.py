433ccffc7082506a592112951d2d28a5f1e74c0b
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2008-03-30 04:07:21 +0000

    some refactoring, better identity handling, but username only passively checks multi-identity

--- trunk/collection.py
+++ trunk/collection.py
@@ -1,7 +1,7 @@
 import util, apis.flickr, apis.delic, apis.technorati, apis.jaiku
-import random
+import random, datetime
 
-entities = []
+entities = {}
 seed = None
 pnetwork = None
 
@@ -22,6 +22,10 @@ class Entity:
 	def spider(self):
 		"""spider is always overidden"""
 		raise RuntimeError, "Must override base Entity spider()"
+
+	def matches(self, other):
+		"""the base match checking"""
+		return self.id == other.id
 
 	def draw(self):
 		if self.active:
@@ -60,23 +64,38 @@ class Tag(Entity):
 	def __init__(self, **kwargs):
 		Entity.__init__(self, ['tag'], **kwargs)
 		self.id = kwargs['tag']
+		# places to store results from api calls
+		self.api_res_flickr = []
+		self.api_res_delic = []
+		self.api_res_tech = []
 
 	def spider(self):
 		ret = []
 		# currently 5 random samples from each of these...
 		limit_to = 5
-		fk = apis.flickr.by_tag(self.tag)
-		for im in random.sample(fk, min(len(fk), limit_to)):
+		# flickr
+		if not self.api_res_flickr:
+			self.api_res_flickr = apis.flickr.by_tag(self.tag)
+			random.shuffle(self.api_res_flickr)
+		for i in range(min(limit_to, len(self.api_res_flickr))):
+			im = self.api_res_flickr.pop(0)
 			ret.append(Image(img_link=im['link'], username=im['author'], author_id=im['author_id'], title=im['title'], tags=im['tags']))
-		dl = apis.delic.by_tag(self.tag)
-		for url in random.sample(dl, min(len(dl), limit_to)):
+		# del.icio.us
+		if not self.api_res_delic:
+			self.api_res_delic = apis.delic.by_tag(self.tag)
+			random.shuffle(self.api_res_delic)
+		for i in range(min(limit_to, len(self.api_res_delic))):
+			url = self.api_res_delic.pop(0)
 			ret.append(Link(url=url['link'], title=url['title']))
-		tc = apis.technorati.by_tag(self.tag)
-		for blog in random.sample(tc, min(len(tc), limit_to)):
+		# technorati
+		if not self.api_res_tech:
+			self.api_res_tech = apis.technorati.by_tag(self.tag)
+			random.shuffle(self.api_res_tech)
+		for i in range(min(limit_to, len(self.api_res_tech))):
+			blog = self.api_res_tech.pop(0)
 			ret.append(BlogPost(blog_link=blog['link'], title=blog['title'], summary=blog.get('summary', '')))
 		print "--- a Tag (%s) spiders %s other entities..." % (self.tag, len(ret))
 		return ret
-		
 
 class BlogPost(Entity):
 	def __init__(self, **kwargs):
@@ -100,106 +119,153 @@ class UserName(Entity):
 		Entity.__init__(self, ['names'], **kwargs)
 		self.id = reduce(lambda n, n2 : n + n2, self.names.values()) # just has to be a unique id
 		self.limit_to = 5
+		# storage for results from api calls:
+
+	class api_res:
+		class flickr:
+			socnet = []
+			images = []
+		class delic:
+			socnet = []
+			links = []
+		class jaiku:
+			socnet = []
+			stuff = []
 
 	def spider(self):
 		ret = []
 		self.network_count = 0
+		self.prep_spider()
 		ret = ret + self.spider_flickr()
 		ret = ret + self.spider_delic()
 		ret = ret + self.spider_jaiku()
-		# TODO more possibilities in this spider... jaiku?
 		####
 		if self.network_count == 0:
 			self.spiderable = False # friendless usernames have been leading me into sandpits
 		else:
 			print "--- a UserName (%s) spiders %s other entities..." % (self.id, len(ret))
 		return ret
+	
+	def matches(self, other):
+		"""cross reference names used"""
+		# as long as we're careful in collection.spider()
+		# self will already part of the collection, while the other is not. So self may have 
+		# multiple services already but other should just have one
+		if other.names.has_key('flickr_id'):
+			if self.names.has_key('flickr_id'): 
+				return other.names.get('flickr_id') == self.names.get('flickr_id')
+			# for flickr, try matching on url-name, which other may not have (url names are assigned flickr_id if not set)
+			if other.names.has_key('flickr_url'):
+				for k, v in self.names.iteritems():
+					if v == other.names.get('flickr_url'):
+						print "^^^ claimed a flickr user via %s = %s" % (k, v)
+						self.names.update(other.names)
+						return True
+			return False # only 1 service per other
+		if other.names.has_key('del.icio.us'):
+			if self.names.has_key('del.icio.us'): 
+				return other.names.get('del.icio.us') == self.names.get('del.icio.us')
+			for k, v in self.names.iteritems():
+				if k in ['flickr_id', 'flickr_display'] : continue # these aren't formats for del.icio.us names
+				if v == other.names.get('del.icio.us'):
+					print "^^^ claimed a del.icio.us user via %s = %s" % (k, v)
+					self.names.update(other.names)
+					return True
+			return False
+		if other.names.has_key('jaiku'):
+			if self.names.has_key('jaiku'):
+				return other.names.get('jaiku') == self.names.get('jaiku')
+			for k, v in self.names.iteritems():
+				if k in ['flickr_id', 'flickr_display'] : continue # these aren't formats for jaiku names
+				if v == other.names.get('jaiku'):
+					print "^^^ claimed a jaiku user via %s = %s" % (k, v)
+					self.names.update(other.names)
+					return True
+		return False
+
+	def prep_spider(self):
+		"""make api calls for services user has, but we have no data"""
+		if self.names.has_key('flickr_id'):
+			if not self.api_res.flickr.socnet:
+				self.api_res.flickr.socnet = apis.flickr.social_network(self.names.get('flickr_id'))
+				random.shuffle(self.api_res.flickr.socnet)
+			if not self.api_res.flickr.images:
+				self.api_res.flickr.images = apis.flickr.user_images(self.names.get('flickr_id'))
+				random.shuffle(self.api_res.flickr.images)
+		if self.names.has_key('del.icio.us'):
+			if not self.api_res.delic.socnet:
+				self.api_res.delic.socnet = apis.delic.social_network(self.names.get('del.icio.us'))
+				random.shuffle(self.api_res.delic.socnet)
+			if not self.api_res.delic.links:
+				self.api_res.delic.links = apis.delic.links(self.names.get('del.icio.us'))
+				random.shuffle(self.api_res.delic.links)
+		if self.names.has_key('jaiku'):
+			if not self.api_res.jaiku.socnet:
+				self.api_res.jaiku.socnet = apis.jaiku.social_network(self.names.get('jaiku'))
+				random.shuffle(self.api_res.jaiku.socnet)
+			if not self.api_res.jaiku.stuff:
+				self.api_res.jaiku.socnet = apis.jaiku.stuff(self.names.get('jaiku'))
+				random.shuffle(self.api_res.jaiku.stuff)
 
 	def spider_flickr(self):
 		ret = []
-		if not self.names.has_key('flickr_id'):
-			for k, v in self.names.iteritems():
-				id = apis.flickr.check_for_user(v) # make an attempt to find on flickr
-				if id:
-					self.names['flickr_id'] = id
-					self.names['flickr_url'] = v
-					break
-		if self.names.has_key('flickr_id'):
-			# spider flickr network
-			fnet = apis.flickr.social_network(self.names.get('flickr_id'))
-			self.network_count = self.network_count + len(fnet)
-			for u in random.sample(fnet, min(len(fnet), self.limit_to)):
-				dnames = {'flickr_display' : u['author'].split('(')[1][:-1]}
-				dnames['flickr_id'] = u['author_id']
-				linkname = u['link'].split("/")[4] # when splitting that url, linkname comes after ['http:', '', 'www.flickr.com', 'photos'
-				if linkname != u['author_id'] : dnames['flickr_url'] = linkname
-				ret.append(UserName(names=dnames))
-			# spider photos for user
-			fph = apis.flickr.user_images(self.names.get('flickr_id'))
-			for im in random.sample(fph, min(len(fph), self.limit_to)):
-				ret.append(Image(img_link=im['link'], username=im['author'], author_id=im['author_id'], title=im['title'], tags=im['tags']))
+		netct = min(self.limit_to, len(self.api_res.flickr.socnet))
+		self.network_count = self.network_count + netct
+		for i in range(netct):
+			u = self.api_res.flickr.socnet.pop(0)
+			dnames = {'flickr_display' : u['author'].split('(')[1][:-1]}
+			dnames['flickr_id'] = u['author_id']
+			linkname = u['link'].split("/")[4] # when splitting that url, linkname comes after ['http:', '', 'www.flickr.com', 'photos'
+			if linkname != u['author_id'] : dnames['flickr_url'] = linkname
+			ret.append(UserName(names=dnames))
+		# spider photos for user
+		for i in range(min(self.limit_to, len(self.api_res.flickr.images))):
+			im = self.api_res.flickr.images.pop(0)
+			ret.append(Image(img_link=im['link'], username=im['author'], author_id=im['author_id'], title=im['title'], tags=im['tags']))
 		return ret
 
 	def spider_delic(self):
 		ret = []
-		dnet = []
-		if not self.names.has_key('del.icio.us'):
-			for k, v in self.names.iteritems():
-				if k in ['flickr_id', 'flickr_display'] : continue # these aren't formats for del.icio.us names
-				dnet = apis.delic.social_network(v)
-				if len(dnet) > 0:
-					# we found a del.icio.us user
-					print "^^^ found a del.icio.us user with %s = %s" % (k, v)
-					self.names['del.icio.us'] = v
-					break
-			if not dnet : return []
-		else:
-			dnet = apis.delic.social_network(self.names.get('del.icio.us'))
-		self.network_count = self.network_count + len(dnet)
-		for u in random.sample(dnet, min(len(dnet), self.limit_to)):
-			# just returns a list of usernames
+		netct = min(self.limit_to, len(self.api_res.delic.socnet))
+		self.network_count = self.network_count + 1 # we'll pass del.icio.us users
+		for i in range(netct):
+			u = self.api_res.delic.socnet.pop(0)
 			ret.append(UserName(names={'del.icio.us' : u}))
-		dln = apis.delic.links(self.names.get('del.icio.us'))
-		for l in random.sample(dln, min(len(dln), self.limit_to)):
+		for i in range(min(self.limit_to, len(self.api_res.delic.links))):
+			l = self.api_res.delic.links.pop(0)
 			ret.append(Link(url=l['link'], title=l['title']))
 		return ret
 
 	def spider_jaiku(self):
 		ret = []
-		if not self.names.has_key('jaiku'):
-			for k, v in self.names.iteritems():
-				if k in ['flickr_id', 'flickr_display'] : continue # these aren't formats for del.icio.us names
-				jnet = apis.jaiku.social_network(v)
-				if len(jnet) > 0:
-					print "^^^ found a jaiku user with %s = %s" % (k, v)
-					self.names['jaiku'] = v
-					break
-			if not jnet : return []
-		else:
-			jnet = apis.jaiku.social_network(self.names.get('jaiku'))
-		self.network_count = self.network_count + len(jnet)
-		for u in random.sample(jnet, min(len(jnet), self.limit_to)):
+		netct = min(self.limit_to, len(self.api_res.jaiku.socnet))
+		self.network_count = self.network_count + netct
+		for i in range(netct):
+			u = self.api_res.jaiku.socnet.pop(0)
 			ret.append(UserName(names={'jaiku' : u['nick']}))
-		jstuff = apis.jaiku.stuff(self.names.get('jaiku'))
-		for s in random.sample(jstuff, min(len(jstuff), self.limit_to)):
+		for i in range(min(self.limit_to, len(self.api_res.jaiku.stuff))):
+			s = self.api_res.jaiku.stuff.pop(0)
 			# different kind of objects for different stuff
 			if s['url'].find("flickr.com") >= 0:
 				print "got Image from jaiku feed"
 				ret.append(Image(**apis.flickr.photo_data(s['url'])))
 			# add twitter here
 		return ret
-			
 
 class Link(Entity):
 	def __init__(self, **kwargs):
 		Entity.__init__(self, ['url', 'title'], **kwargs)
 		self.id = kwargs['url']
+		self.api_res_delic = []
 
 	def spider(self):
 		ret = []
 		limit_to = 5
-		udat = apis.delic.url_data(self.url)
-		for dat in random.sample(udat, min(len(udat), limit_to)):
+		if not self.api_res_delic:
+			self.api_res_delic = apis.delic.url_data(self.url)
+			random.shuffle(self.api_res_delic)
+		for i in range(min(limit_to, len(self.api_res_delic))):
+			dat = self.api_res_delic.pop(0)
 			tags_added = 0 
 			for tdict in dat['tags']:
 				for t in tdict['term'].split(' '):
@@ -215,14 +281,14 @@ class Link(Entity):
 def spider():
 	global entities, seed, pnetwork
 	if not seed: 
-		if len(entities) > 0 : raise RuntimeError, "spider without argument only to initialize"
+		if len(entities.keys()) > 0 : raise RuntimeError, "spider without argument only to initialize"
 		entity = Tag(tag='identity')
 		entity.index = 0
-		entities.append(entity)
+		entities[str(entity.__class__)] = [entity]
 	else:
 		entity = seed
 	entity.active = True
-	print '\n\n__main spider() call on entity id=%s [%s]__' % (entity.id, entity.__class__)
+	print '\n\n__main spider() call on entity id=%s [%s]__ %s' % (entity.id, entity.__class__, datetime.datetime.today().strftime("%a %I:%M%p"))
 	network = entity.spider() 
 	ncount = len(network)
 	if ncount == 0 or not entity.spiderable:
@@ -245,11 +311,16 @@ def spider():
 def get_or_add(entity):
 	"""if the entity is in the collection already, return the collected one. Otherwise add it to collection
 	returns boolean added, and obj"""
-	for e in entities:
-		if e.id == entity.id and e.__class__ == entity.__class__: return False, e
-	entity.index = len(entities)
-	entities.append(entity)
-	return True, entity
+	ekey = str(entity.__class__)
+	if entities.has_key(ekey):
+		for e in entities[ekey]:
+			if e.matches(entity): return False, e
+		entity.index = len(entities[ekey])
+		entities[ekey].append(entity)
+		return True, entity
+	else:
+		entities[ekey] = [entity]
+		return True, entity
 
 if __name__ == '__main__':
 	util.log('INIT')
