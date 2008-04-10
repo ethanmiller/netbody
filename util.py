5f4ac99897d4bc1e51bfe31333d7faf623da4e96
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2008-04-10 07:37:22 +0000

    this draws the network (just barely), and resizes the frames before output

--- util.py
+++ util.py
@@ -6,6 +6,11 @@ class CONST:
 	WIN_WIDTH = 2560 # we'll scale down to 1280x720
 	WIN_HEIGHT = 1440
 	NOISE_WORDS = "about,after,all,also,an,and,another,any,are,as,at,be,because,been,before,being,between,both,but,by,came,can,come,could,did,do,each,for,from,get,got,has,had,he,have,her,here,him,himself,his,how,if,in,into,is,it,like,make,many,me,might,more,most,much,must,my,never,now,of,on,only,or,other,our,out,over,said,same,see,should,since,some,still,such,take,than,that,the,their,them,then,there,these,they,this,those,through,to,too,under,up,very,was,way,we,well,were,what,where,which,while,who,with,would,you,your".split(",")
+	GRID_SPACE = 30
+	CURVE_X_VAR_MIN = 50
+	CURVE_X_VAR_MAX = 200
+	CURVE_SEG_MIN = 50
+	CURVE_SEG_MAX = 200
 
 def sizeimg(im):
 	if im.size[0] >= im.size[1]:
@@ -46,3 +51,34 @@ def unicode_enforce(obj, encoding='utf-8'):
 		if not isinstance(obj, unicode):
 			obj = unicode(obj, encoding)
 	return obj
+
+def calculate_bezier(p, steps=30):
+	"""
+	Calculate a bezier curve from 4 control points and return a 
+	list of the resulting points.
+
+	The function uses the forward differencing algorithm described here: 
+	http://www.niksula.cs.hut.fi/~hkankaan/Homepages/bezierfast.html
+	"""
+
+	t = 1.0 / steps
+	temp = t*t
+
+	f = p[0]
+	fd = 3 * (p[1] - p[0]) * t
+	fdd_per_2 = 3 * (p[0] - 2 * p[1] + p[2]) * temp
+	fddd_per_2 = 3 * (3 * (p[1] - p[2]) + p[3] - p[0]) * temp * t
+
+	fddd = fddd_per_2 + fddd_per_2
+	fdd = fdd_per_2 + fdd_per_2
+	fddd_per_6 = fddd_per_2 * (1.0 / 3)
+
+	points = []
+	for x in range(steps):
+		points.append(f)
+		f = f + fd + fdd_per_2 + fddd_per_6
+		fd = fd + fdd + fddd_per_2
+		fdd = fdd + fddd
+		fdd_per_2 = fdd_per_2 + fddd_per_2
+	points.append(f)
+	return points
