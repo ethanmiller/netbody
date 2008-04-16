8de5b556840b30b88c57dae39999adefc3bfeb1e
thnntn <thnntn@9c055c7b-9d29-0410-bcad-4118a2cb9d5c>
2008-04-16 18:28:37 +0000

    curses on missing keys

--- trunk/collection/entities.py
+++ trunk/collection/entities.py
@@ -347,7 +347,7 @@ class UserName(base.Entity):
 		# just get 2 vids
 		for i in range(min(2, len(self.api_res.yt.vids))):
 			yt = self.api_res.yt.vids.pop(0)
-			ret.append(Video(url=yt['link'], title=yt['title'], username=yt['media_credit'], tags=yt['media_category']))
+			ret.append(Video(url=yt['link'], title=yt['title'], username=yt.get('media_credit', 'none'), tags=yt.get('media_category', '')))
 		self.network_count = self.network_count + len(ret)
 		return ret
 
